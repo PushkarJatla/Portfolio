ae9486c2fcbf92df3aad68e355a707f3cf83fdb6
PushkarJatla <pushkarjatla02@gmail.com>
2025-07-14 10:48:05 -0700

    added Updated CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/PushkarJatlaResume.pdf"
+                href="/PushkarJatlaMERN.pdf"
                 download
 
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
