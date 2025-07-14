301747b2428d88b615878a4c8fbf589f0c371a9b
PushkarJatla <pushkarjatla02@gmail.com>
2025-07-14 11:03:36 -0700

    dark theme color added

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,16 +5,20 @@ import { cn } from "@/lib/utils";
 export const ThemeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(true);
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
-      setIsDarkMode(false);
-    }
-  }, []);
+useEffect(() => {
+  const storedTheme = localStorage.getItem("theme");
+
+  if (storedTheme === "dark" || storedTheme === null) {
+    // If it's set to dark OR there's no theme stored yet, default to dark
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    setIsDarkMode(true);
+  } else {
+    document.documentElement.classList.remove("dark");
+    setIsDarkMode(false);
+  }
+}, []);
+
 
   const toggleTheme = () => {
     if (isDarkMode) {
